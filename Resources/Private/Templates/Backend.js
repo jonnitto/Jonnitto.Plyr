7e252dda3f14ccad9b227de13edc38b1c2ff89be
Jon Uhlmann <me@jonnitto.ch>
2017-12-01 14:47:53 +0100

    :bug: Make Plyr also work in the new UI

--- Resources/Private/Templates/Backend.js
+++ Resources/Private/Templates/Backend.js
@@ -1,19 +1,31 @@
 (function() {
-    var events = ["PageLoaded", "NodeCreated"];
-    var files = [
-        "https://player.vimeo.com/api/player.js",
-        "/_Resources/Static/Packages/Jonnitto.Plyr/Main.js"
-    ];
-    document.addEventListener("Neos.ContentModuleLoaded", function() {
-        setTimeout(function() {
-            require(files, function(Vimeo, plyr) {
-                window.Vimeo = { Player: Vimeo };
-                window.plyr = plyr;
-                initPlyr();
-                for (var i = 0; i < events.length; i++) {
-                    document.addEventListener("Neos." + events[i], initPlyr);
-                }
-            });
-        }, 100);
-    });
+    if (typeof require != "undefined") {
+        var events = ["PageLoaded", "NodeCreated"];
+        var files = [
+            "https://player.vimeo.com/api/player.js",
+            "/_Resources/Static/Packages/Jonnitto.Plyr/Main.js"
+        ];
+        document.addEventListener("Neos.ContentModuleLoaded", function() {
+            setTimeout(function() {
+                require(files, function(Vimeo, plyr) {
+                    window.Vimeo = { Player: Vimeo };
+                    window.plyr = plyr;
+                    initPlyr();
+                    for (var i = 0; i < events.length; i++) {
+                        document.addEventListener(
+                            "Neos." + events[i],
+                            initPlyr
+                        );
+                    }
+                });
+            }, 100);
+        });
+    } else {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.setAttribute('src','/_Resources/Static/Packages/Jonnitto.Plyr/Main.js');
+        script.onload = initPlyr;
+        document.head.appendChild(script);
+    }
 })();
