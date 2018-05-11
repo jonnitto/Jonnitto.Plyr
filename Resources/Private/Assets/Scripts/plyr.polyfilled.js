0a750be5782c296c135ba487b6f1ddba708fcf0a
Jon Uhlmann <me@jonnitto.ch>
2018-05-11 21:49:29 +0200

    :arrow_up: Update to Plyr 3.3.7

--- Resources/Private/Assets/Scripts/plyr.polyfilled.js
+++ Resources/Private/Assets/Scripts/plyr.polyfilled.js
@@ -1,14 +1,12 @@
 // ==========================================================================
 // Plyr Polyfilled Build
-// plyr.js v3.1.0
+// plyr.js v3.3.7
 // https://github.com/sampotts/plyr
 // License: The MIT License (MIT)
 // ==========================================================================
 
 import 'babel-polyfill';
-
 import 'custom-event-polyfill';
-
 import Plyr from './plyr';
 
 export default Plyr;
